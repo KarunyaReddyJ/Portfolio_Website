f18a8bee55309d8ec40e0b68dc3f85a07272b07d
KarunyaReddyJ <Karunyareddy1418@gmail.com>
2024-05-27 17:20:12 +0530

    made changes to package.json,vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // This will use your machine's default IP address
+    host: true,
+    'Permissions-Policy': 'interest-cohort=()'
+ // This will use your machine's default IP address
   },
   base: '/Portfolio_Website/'
 })
